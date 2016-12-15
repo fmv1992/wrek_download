9721defafd499f1384765ca22b5c7a65d313f3f1
monteiro <fmv1992@gmail.com>
2016-12-15 09:50:58 -0200

    expanding test suite

--- tests/wrek_tests.py
+++ tests/wrek_tests.py
@@ -3,7 +3,9 @@ u"""Test for WREK download program."""
 import unittest
 import random
 import os
+import tempfile
 from wrek_download import aux_functions
+from wrek_download.parse_wrek_website import initialize_shows
 
 # Ignore setting attribute outside init.
 # pylama: ignore:W0201
@@ -27,6 +29,28 @@ class AuxiliarFunctionsTestCase(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             aux_functions.create_whitelist(
                 self.non_existent_file_path)
+
+
+class WREKShowTestCase(unittest.TestCase):
+
+    u"""Test that an mp3 file was downloaded."""
+
+    def setUp(self):
+        u"""Set up test framework."""
+        self.wrek_programs = initialize_shows()
+        self.archive_folder = tempfile.TemporaryDirectory()
+        self.temporary_folder = tempfile.TemporaryDirectory()
+        self.output_folder = tempfile.TemporaryDirectory()
+        self.program = None
+
+class AtmosphericsTestCase(WREKShowTestCase):
+
+    u"""Test that an Atmospherics mp3 file was downloaded."""
+
+    def setUp(self):
+        self.program = [x for x in self.wrek_programs if 'atmospherics' in x][0]
+
+
 
 if __name__ == '__main__':
     unittest.main()
